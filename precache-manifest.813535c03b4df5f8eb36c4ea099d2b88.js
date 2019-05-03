365becf1b614d1da27f1a1e8ad15d14d26c95e99
Denys Periel <denys@periel.biz>
2019-05-03 21:04:40 +0200

    Updates

---
+++ precache-manifest.813535c03b4df5f8eb36c4ea099d2b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69196091a8cc479870e37de8a709c9fb",
+    "url": "/infinite-scroll/index.html"
+  },
+  {
+    "revision": "0957520be4a32e913087",
+    "url": "/infinite-scroll/static/css/main.ea64b213.chunk.css"
+  },
+  {
+    "revision": "22e147dafea70906b7b4",
+    "url": "/infinite-scroll/static/js/2.483d3744.chunk.js"
+  },
+  {
+    "revision": "0957520be4a32e913087",
+    "url": "/infinite-scroll/static/js/main.1538949d.chunk.js"
+  },
+  {
+    "revision": "674925d4062fb7d0003c",
+    "url": "/infinite-scroll/static/js/runtime~main.ff4c60e7.js"
+  }
+]);
